591c3f511eb6f314b7dda7ccfd5f59f2e0112707
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-09 20:15:16 +0100

    fix: correct MongoDB connection URI variable name

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
