ae9e2e184c0d40edfd32883f16708f6705b40b78
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-09 20:53:26 +0100

    feat: add verification fields to User schema and remove unused temp file

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,25 @@ const register = async (req, res) => {
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
+  
+  const verificationToken = 'fake token';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken
+  })
+
+//send verification token back only while testing in postman!!!
+
+  res
+  .status(StatusCodes.CREATED)
+  .json({ 
+    msg: 'Sucess! please check your email to verify account',
+    verificationToken: user.verificationToken,
+  });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
