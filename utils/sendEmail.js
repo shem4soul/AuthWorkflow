03b3a2539516ca3fbbe98d58bb3bb06306411683
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-17 19:07:44 +0100

    sendemail update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,19 @@
  const nodemailer = require('nodemailer')
+ const nodemailerConfig = require('./nodemailerConfig')
 
- const sendEmail = async()  =>{
+ const sendEmail = async({to, subject, html})  =>{
  let testAccount = await nodemailer.createTestAccount()
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  auth: {
-    user: "russel.schmitt55@ethereal.email",
-    pass: "JTs6Eve4T7HCfz8upX",
-  },
-});
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+  return transporter.sendMail({
+    from: '"Coding Addict" <codingaddict@gmail.com>',
+    to,
+    subject,
+    html,
+  });
 
 }
+
+
+module.exports = sendEmail
