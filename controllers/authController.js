12e840aebf460423e400585c179c5a847d133cb2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-21 10:45:21 +0100

    complete token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Token = require('../models/Token')
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require('../utils');
@@ -108,11 +109,24 @@ if (!user.isVerified) {
 }
 
   const tokenUser = createTokenUser(user);
- 
+
+  // create refresh token 
+
+  let refreshToken = '';
+// check for existing token
+
+
+
+refreshToken = crypto.randomBytes(40).toString('hex')
+const userAgent = req.headers['user-agent'];
+const ip = req.ip;
+const userToken = {refreshToken, ip, userAgent, user: user._id};
+
+const token = await Token.create(userToken)
 
   // attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser, token });
 };
 
 
